2c73146f9e549aa78718a175234f550c248cdf47
Pukspuu <kaarelkor@gmail.com>
2023-02-03 09:51:47 +0200

    uus

--- loeng07/kodutoo2.js
+++ loeng07/kodutoo2.js
@@ -1,11 +1,10 @@
 let nimi
 nimi=prompt('Mis su nimi on?')
-let valjundElement = document.getElementById('valjund')
+let valjundElement = document.getElemekmntById('valjund')
 valjundElement.innerHTML = 'Sinu nimi tagurpidi: '
 
 for (let i = nimi.length-1; i >=0; i--){
     valjundElement.innerHTML += nimi[i]
-
 }
 
 
