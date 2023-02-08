4d7afc107b53755f7fb4d1afa54acd1af43b2e61
Pukspuu <kaarelkor@gmail.com>
2023-02-08 16:45:02 +0200

    up

--- Matkaklubi/uudised.js
+++ Matkaklubi/uudised.js
@@ -1,21 +1,21 @@
 let uudised = [
     {
         pealkiri: 'Uued tuuled-uued kuued',
-        autor: 'Onu Uno',
+        autor: 'Autor: Onu Uno',
         artikkel: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ratione veniam provident dolorem laboriosam officiis repellat distinctio. Quos voluptate ea corporis praesentium iusto atque excepturi, repellendus molestias, quibusdam dolor mollitia. Recusandae.',
         pilt: './assets/windy.jpg'
     },
 
     {
         pealkiri: 'Langevarjuga või langevarjuta?',
-        autor: 'Onu Kuuno',
+        autor: 'Autor: Onu Kuuno',
         artikkel: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ratione veniam provident dolorem laboriosam officiis repellat distinctio. Quos voluptate ea corporis praesentium iusto atque excepturi, repellendus molestias, quibusdam dolor mollitia. Recusandae.',
         pilt: './assets/langevari.jpg'
     },
 
     {
         pealkiri: 'Maalt ja merelt',
-        autor: 'Onu Aadu',
+        autor: 'Autor: Onu Aadu',
         artikkel: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ratione veniam provident dolorem laboriosam officiis repellat distinctio. Quos voluptate ea corporis praesentium iusto atque excepturi, repellendus molestias, quibusdam dolor mollitia. Recusandae.',
         pilt: './assets/maalt.jpg'
     },
@@ -24,14 +24,18 @@ let uudised = [
 function koostaUudiseHTML(uudis) {
     return `
     <div class="card">
-        <img class="card-img-top" src="${uudis.pilt}" alt="">
+       
         <div class="card-body">
 
-            <h3>${uudis.pealkiri}</h3>
+            <h4>${uudis.pealkiri}</h4>
             <p>
-            <h4>${uudis.autor}<h4>
+            <h5>${uudis.autor}<h5>
             </p>
-                ${uudis.artikkel}
+            <p>    ${uudis.artikkel} </p>
+            
+            <img class="card-img-top" src="${uudis.pilt}" alt=""> 
+ 
+               
         </div>
     </div>    
     `
