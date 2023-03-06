c73870543a0bd2c4438b34e40f57785dfe1274a2
Pukspuu <kaarelkor@gmail.com>
2023-03-06 13:06:53 +0200

    cfswedvf

--- loeng13/server/index.js
+++ loeng13/server/index.js
@@ -1,20 +1,24 @@
 var express = require('express');
+var cors = require('cors');
 var app = express();
+app.use(cors());
+app.use(express.json());
 
 let uusID= 0;
 let todoItems = [];
 
-app.put('./', function(req,res){
-   console.log('olen put');
+app.put('./', function(req, res){
+todoItems.push({
+      id: uusId++,
+      ischecked: false,
+      text: req.body.text
+   });
    res.send(JSON.stringify(todoItems));
 });
 
 
 app.get('/', function (req, res) {
-   res.set({
-      "Content-Type": "application/json",
-      "Access-Control-Allow-Origin": "*"
-}); 
+ 
    res.send(JSON.stringify(todoItems));
 });
 
