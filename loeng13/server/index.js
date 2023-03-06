134c1e0c19e7e7cd95acf40323a7bfd1772e08f3
Pukspuu <kaarelkor@gmail.com>
2023-03-06 11:12:44 +0200

    Update index.js

--- loeng13/server/index.js
+++ loeng13/server/index.js
@@ -3,11 +3,15 @@ var app = express();
 
 
 app.get('/', function (req, res) {
-
+   res.set({
+      'Content-Type': 'application/json',
+      "Access-Control-Allow-Origin": "*"
+}); 
+   res.writeHead(200);
+   res.send('tere');
 })
 
 var server = app.listen(8081, function () {
-   var host = server.address().address
    var port = server.address().port
-   console.log("Example app listening at http://%s:%s", host, port)
+   console.log("Example app listening at http://localhost:%s", port)
 })
