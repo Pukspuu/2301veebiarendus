f2af060d87704757110a3b7701f2fd5161136e05
Pukspuu <kaarelkor@gmail.com>
2023-03-06 12:19:46 +0200

    sdd

--- loeng13/server/index.js
+++ loeng13/server/index.js
@@ -1,17 +1,24 @@
 var express = require('express');
 var app = express();
 
+let uusID= 0;
+let todoItems = [];
+
+app.put('./', function(req,res){
+   console.log('olen put');
+   res.send(JSON.stringify(todoItems));
+});
+
 
 app.get('/', function (req, res) {
    res.set({
-      'Content-Type': 'application/json',
+      "Content-Type": "application/json",
       "Access-Control-Allow-Origin": "*"
 }); 
-   res.writeHead(200);
-   res.send('tere');
-})
+   res.send(JSON.stringify(todoItems));
+});
 
 var server = app.listen(8081, function () {
    var port = server.address().port
    console.log("Example app listening at http://localhost:%s", port)
-})
+}); 
